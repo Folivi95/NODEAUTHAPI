effe55f8fafb7596b4eba433ebd833874b5b3734
Folivi95 <joshua.folivi@gmail.com>
2019-09-27 14:25:57 +0100

    added support for token expiration

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/api/login', (req, res) => {
         email: 'test@test.com'
     };
 
-    jwt.sign({user}, 'secretKey', (err, token) => {
+    jwt.sign({user}, 'secretKey', {expiresIn: '30s'}, (err, token) => {
         res.json({
             token
         });
